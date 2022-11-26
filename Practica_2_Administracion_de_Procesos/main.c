8eb2c65a6694c96745b0f22df3ffc9d0e9ca6ae5
Pitaro Dark <dark.2002ikari@gmail.com>
2022-11-25 18:48:38 -0600

    Añadimos memoria compartida

--- Practica_2_Administracion_de_Procesos/main.c
+++ Practica_2_Administracion_de_Procesos/main.c
@@ -1,9 +1,27 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
 
-int main(){
+#define SHMKEY 75
 
-    puts("Hola Mundo, Soy Pitaro Dark");
-
+int main()
+{
+    int shmid;
+    char *addr, *pint;
+    /* Crear la región de memoria y obtener la dirección */
+    shmid = shmget(SHMKEY, 1, 0777 | IPC_CREAT);
+    /* Enlazar región al espacio de direccionamiento del proceso */
+    addr = shmat(shmid, 0, 0);
+    pint = (char *)addr; /* Reservar addr */
+    /* Código de la sincronización */
+    sleep(10); /* Para dar tiempo a que se ejecute el segundo proceso */
+    (*pint)++;
+    /* Separar la región del espacio de direccionamiento del proceso */
+    shmdt(addr);
+    /* Eliminar la región de memoria compartida */
+    shmctl(shmid, IPC_RMID, 0); /* Esta operación también la puede realizar otro proceso */
     return 0;
 }
